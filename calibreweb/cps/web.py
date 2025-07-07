64b7902f68722ebed20c01b78586ef974298c2c0
michaelalausova <misa.lausova@gmail.com>
2025-07-07 20:35:58 +0200

    nové knihy 7.7.2025

--- calibreweb/cps/web.py
+++ calibreweb/cps/web.py
@@ -952,23 +952,20 @@ def author_list():
         abort(404)
 
 
-@web.route("/downloadlist")
+@web.route("/newbooks")
 @login_required_if_no_ano
-def download_list():
-    if current_user.get_view_property('download', 'dir') == 'desc':
-        order = ub.User.name.desc()
-        order_no = 0
-    else:
-        order = ub.User.name.asc()
-        order_no = 1
-    if current_user.check_visibility(constants.SIDEBAR_DOWNLOAD) and current_user.role_admin():
-        entries = ub.session.query(ub.User, func.count(ub.Downloads.book_id).label('count')) \
-            .join(ub.Downloads).group_by(ub.Downloads.user_id).order_by(order).all()
-        char_list = ub.session.query(func.upper(func.substr(ub.User.name, 1, 1)).label('char')) \
-            .filter(ub.User.role.op('&')(constants.ROLE_ANONYMOUS) != constants.ROLE_ANONYMOUS) \
-            .group_by(func.upper(func.substr(ub.User.name, 1, 1))).all()
-        return render_title_template('list.html', entries=entries, folder='web.books_list', charlist=char_list,
-                                     title=_("Downloads"), page="downloadlist", data="download", order=order_no)
+def new_books():
+    if current_user.check_visibility(constants.SIDEBAR_DOWNLOAD):
+        thirty_days_ago = datetime.utcnow() - timedelta(days=30)
+        entries = ub.session.query(ub.Books) \
+            .filter(ub.Books.visible == True) \
+            .filter(ub.Books.timestamp >= thirty_days_ago) \
+            .order_by(ub.Books.timestamp.desc()) \
+            .all()
+        return render_title_template("list.html", entries=entries,
+                                     folder="web.books_list", charlist=None,
+                                     title=_("New Books"),
+                                     page="newbooks", data="new", order="new")
     else:
         abort(404)
 
